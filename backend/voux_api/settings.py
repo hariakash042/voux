32642fb4740247c9653dc05b1cd753070df7dfb6
Akash Hari <hariakash042@gmail.com>
2026-05-12 12:40:35 +0530

    Integrated PostgreSQL and created product database models

--- backend/voux_api/settings.py
+++ backend/voux_api/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'voux_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'voux_db',
+        'USER': 'postgres',
+        'PASSWORD': 'voux1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
