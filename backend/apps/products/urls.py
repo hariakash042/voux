9f5fa02be4b25817566099351c71c3623799493b
Akash Hari <hariakash042@gmail.com>
2026-05-12 15:15:34 +0530

    Implemented full CRUD product APIs and Postman testing

--- backend/apps/products/urls.py
+++ backend/apps/products/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
-from .views import product_list
+
+from .views import (
+    product_list,
+    product_detail,
+    create_product,
+    update_product,
+    delete_product,
+)
 
 urlpatterns = [
     path('', product_list),
+
+    path('<int:pk>/', product_detail),
+
+    path('create/', create_product),
+
+    path('<int:pk>/update/', update_product),
+
+    path('<int:pk>/delete/', delete_product),
 ]
