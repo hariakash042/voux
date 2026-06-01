88ca1dfeadb794cf3f17bc5bb94067b27c6fc93a
Akash Hari <hariakash042@gmail.com>
2026-06-01 10:53:13 +0530

    Setup React frontend with TailwindCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <h1 className="text-7xl font-bold p-10">
+        VOUX
+      </h1>
+    </div>
+  )
+}
